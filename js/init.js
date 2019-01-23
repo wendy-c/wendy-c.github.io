14d74b0889561137f96c57021aa9e312151453c7
Wendy <tell.wendy@gmail.com>
2019-01-22 20:27:47 -0800

    jan 19 update

--- js/init.js
+++ js/init.js
@@ -52,27 +52,11 @@ getPie('graph1', 'English');
 getPie('graph2', 'Cantonese');
 getPie('graph3', 'Mandarin');
 
-// Google Map
-function initMap() {
-  var sb = {lat: 37.6305, lng: -122.4111};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: sb,
-    scrollwheel: false
-  });
-  var marker = new google.maps.Marker({
-    position: sb,
-    map: map
-  });
-}
-
 (function($) {
   $(function() {
-    new WOW({mobile: false}).init();
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({full_width: true});
     $('.materialboxed').materialbox();
 
     // animate icon 
@@ -106,66 +90,11 @@ function initMap() {
       {selector: '#about-img', offset: 50, callback: function(el) {
         Materialize.fadeInImage($(el));
       } },
-      {selector: '#resume', offset: 205, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
-      {selector: '#projects', offset: 400, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
       {selector: '#image-test', offset: 500, callback: function(el) {
         Materialize.fadeInImage($(el));
       } }
     ];
     Materialize.scrollFire(options);
-
-
-    
-    // *---project photo slider---*
-
-    var slideIndex = 1;
-    showDivs(slideIndex);
-
-    function plusDivs(n, current) {
-        showDivs(slideIndex += n, current);
-    }
-
-    function showDivs(n, current) {
-      var i;
-      if (current) {
-        current = 'p-img' + current;
-      } else {
-        current = 'p-img1';
-      }
-      var x = document.getElementsByClassName(current);
-      if (n > x.length) {
-        slideIndex = 1;
-      } 
-      if (n < 1) {
-        slideIndex = x.length;
-      }
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = 'none'; 
-      }
-      x[slideIndex - 1].style.display = 'block'; 
-    }
-
-    $('.display-left').on('click', function() {
-      var currentClass = $(this).data('val');
-      console.log(currentClass, 'wat is current class?');
-      plusDivs(-1, currentClass);
-    });
-    $('.display-right').on('click', function() {
-      var currentClass = $(this).data('val');
-      plusDivs(1, currentClass);
-    });
-
-    // weatherman modal
-    $('.p-mini1').on('click', function() {
-      $('.weatherman').css('display', 'block');
-    });
-    $('.close-m1').on('click', function() {
-      $('.weatherman').css('display', 'none');
-    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
